2f6edb8d77cb55a9270c740396314576618e47c1
CF Bolz-Tereick <cfbolz@gmx.de>
2026-01-08 21:31:29 +0100

    support --jit off

--- targetpydull.py
+++ targetpydull.py
@@ -1,7 +1,18 @@
+from rpython.rlib import jit
+
 from pydull.parser import parse
 from pydull.interpreter import run
 
 def main(args):
+    for i in range(len(args)):
+        if args[i] == "--jit":
+            if len(args) == i + 1:
+                print("missing argument after --jit")
+                return 2
+            jitarg = args[i + 1]
+            del args[i:i+2]
+            jit.set_user_param(None, jitarg)
+            break
     if len(args) == 1:
         print("Give me moar arguments")
     filename = args[1]
